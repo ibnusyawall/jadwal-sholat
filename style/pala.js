3c44e2e7671a30312b561f85337326cd047702c1
Ibnu syawal <46734417+ibnusyawall@users.noreply.github.com>
2019-02-07 00:22:24 +0700

    Update pala.js

--- style/pala.js
+++ style/pala.js
@@ -2,9 +2,11 @@
 
 var warna = require('./warna.js');
 var ini = require('../inc/ini.json');
+var waktu = require(../inc/waktu.json')
 console.log("")
 console.log("+-+-+-+-+-+-+ +-+-+-+-+-+-+".hijau + "  C : " + ini.data[0].a)
 console.log("|J|A|D|W|A|L| |S|H|O|L|A|T|".hijau + "  T : " + ini.data[0].b)
 console.log("+-+-+-+-+-+-+ +-+-+-+-+-+-+".hijau + "  N : " + ini.data[0].c)
+console.log("[".kuning + "!".merah + "]".kuning + " Jadwal untuk tanggal : ".kuning + waktu.data[0].waktu.biru)
 console.log("[".kuning + "!".merah + "]".kuning + " Load data ...".kuning)
 
