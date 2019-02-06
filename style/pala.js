24dde8fb6934cdab03db27fe9d1f317527aaae0a
Ibnu syawal <46734417+ibnusyawall@users.noreply.github.com>
2019-02-06 21:04:46 +0700

    Add files via upload

---
+++ style/pala.js
@@ -0,0 +1,8 @@
+var warna = require('./warna.js');
+var ini = require('../inc/ini.json');
+console.log("")
+console.log("+-+-+-+-+-+-+ +-+-+-+-+-+-+".hijau + "  C : " + ini.data[0].a)
+console.log("|J|A|D|W|A|L| |S|H|O|L|A|T|".hijau + "  T : " + ini.data[0].b)
+console.log("+-+-+-+-+-+-+ +-+-+-+-+-+-+".hijau + "  N : " + ini.data[0].c)
+console.log("[".kuning + "!".merah + "]".kuning + " Load data ...".kuning)
+
