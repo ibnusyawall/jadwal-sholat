f530034b829705f331b766d20231c518e0d15de3
Ibnu syawal <46734417+ibnusyawall@users.noreply.github.com>
2019-02-07 00:25:50 +0700

    Update pala.js

--- style/pala.js
+++ style/pala.js
@@ -7,6 +7,6 @@ console.log("")
 console.log("+-+-+-+-+-+-+ +-+-+-+-+-+-+".hijau + "  C : " + ini.data[0].a)
 console.log("|J|A|D|W|A|L| |S|H|O|L|A|T|".hijau + "  T : " + ini.data[0].b)
 console.log("+-+-+-+-+-+-+ +-+-+-+-+-+-+".hijau + "  N : " + ini.data[0].c)
-console.log("[".kuning + "!".merah + "]".kuning + " Jadwal untuk tanggal : ".kuning + waktu.data[0].waktu.biru)
+console.log("[".kuning + "!".merah + "]".kuning + " Jadwal sholat untuk tanggal : ".kuning + waktu.data[0].waktu.biru)
 console.log("[".kuning + "!".merah + "]".kuning + " Load data ...".kuning)
 
